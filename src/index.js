5afbc091b35212010c2d0eb0f8b89bf77754293c
coffeekosatka <viikkuullyy@gmail.com>
2023-08-21 22:17:50 +0400

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    const text = n.toString();
+    let result = '';
+    let i = text.length - 1;
+    while(i >= 0){
+        if(text[i] === '-'){
+        result = `${result}`;
+        }
+        else{
+        result = `${result}${text[i]}`;
+    }
+        i = i - 1;
+    }
+    return result * 1;
 }
